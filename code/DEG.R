76af1e60e58f19dc10c219180ce6be9cd86c4a88
Medinfo Lad <m18231838586@163.com>
2026-01-09 19:46:03 +0800

    code

--- code/DEG.R
+++ code/DEG.R
@@ -12,24 +12,24 @@ load("data/Meth.RData")
 
 #meth----
 #CpG----
-GSE121690_CpG_genetss2k_meth_data <- Read_file_meth_colname(GSE121690_CpG_genetss2k_meth,"meth")
-GSE121690_CpG_genebody_meth_data <- Read_file_meth_colname(GSE121690_CpG_genebody_meth,"meth")
-GSE121690_CpG_encode_meth_data <- Read_file_meth_colname(GSE121690_CpG_encode_meth,"meth")
+GSE121690_CpG_genetss2k_meth_data <- Read_file_meth_colname(GSE121690_CpG_genetss2k_meth,"site")
+GSE121690_CpG_genebody_meth_data <- Read_file_meth_colname(GSE121690_CpG_genebody_meth,"site")
+GSE121690_CpG_encode_meth_data <- Read_file_meth_colname(GSE121690_CpG_encode_meth,"site")
 
-GSE121690_CpG_genetss2k_UNmeth_data <- Read_file_meth_colname(GSE121690_CpG_genetss2k_meth,"UNmeth")
-GSE121690_CpG_genebody_UNmeth_data <- Read_file_meth_colname(GSE121690_CpG_genebody_meth,"UNmeth")
-GSE121690_CpG_encode_UNmeth_data <- Read_file_meth_colname(GSE121690_CpG_encode_meth,"UNmeth")
+GSE121690_CpG_genetss2k_UNmeth_data <- Read_file_meth_colname(GSE121690_CpG_genetss2k_meth,"UNsite")
+GSE121690_CpG_genebody_UNmeth_data <- Read_file_meth_colname(GSE121690_CpG_genebody_meth,"UNsite")
+GSE121690_CpG_encode_UNmeth_data <- Read_file_meth_colname(GSE121690_CpG_encode_meth,"UNsite")
 
 
 
 #GpC----
-GSE121690_GpC_genetss2k_meth_data <- Read_file_meth_colname(GSE121690_GpC_genetss2k_meth,"meth")
-GSE121690_GpC_genebody_meth_data <- Read_file_meth_colname(GSE121690_GpC_genebody_meth,"meth")
-GSE121690_GpC_encode_meth_data <- Read_file_meth_colname(GSE121690_GpC_encode_meth,"meth")
+GSE121690_GpC_genetss2k_meth_data <- Read_file_meth_colname(GSE121690_GpC_genetss2k_meth,"site")
+GSE121690_GpC_genebody_meth_data <- Read_file_meth_colname(GSE121690_GpC_genebody_meth,"site")
+GSE121690_GpC_encode_meth_data <- Read_file_meth_colname(GSE121690_GpC_encode_meth,"site")
 
-GSE121690_GpC_genetss2k_UNmeth_data <- Read_file_meth_colname(GSE121690_GpC_genetss2k_meth,"UNmeth")
-GSE121690_GpC_genebody_UNmeth_data <- Read_file_meth_colname(GSE121690_GpC_genebody_meth,"UNmeth")
-GSE121690_GpC_encode_UNmeth_data <- Read_file_meth_colname(GSE121690_GpC_encode_meth,"UNmeth")
+GSE121690_GpC_genetss2k_UNmeth_data <- Read_file_meth_colname(GSE121690_GpC_genetss2k_meth,"UNsite")
+GSE121690_GpC_genebody_UNmeth_data <- Read_file_meth_colname(GSE121690_GpC_genebody_meth,"UNsite")
+GSE121690_GpC_encode_UNmeth_data <- Read_file_meth_colname(GSE121690_GpC_encode_meth,"UNsite")
 
 
 group_levels <- levels(factor(GSE121690_CpGgroup$Developmental_stage))
@@ -46,7 +46,7 @@ for (i in 1:length(group_levels)) {
     GSE121690_CpG_genetss2k_meth_data,
     GSE121690_CpG_genetss2k_UNmeth_data,
     GSE121690_CpGgroup,
-    "Developmental_stage","meth","UNmeth",
+    "Developmental_stage","site","UNsite",
     group_suffix)
   genetss2k_DEG_data <- merge(genetss2k_DEG_result, genetss2k_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
@@ -57,28 +57,12 @@ for (i in 1:length(group_levels)) {
   # write.csv(genetss2k_DEG_data,genetss2k_file_suffix_name,row.names = F)
 
 
-  cat("genebody ")
-  genebody_DEG_result <- Meth_group_variance_analysis(
-    GSE121690_CpG_genebody_meth_data,
-    GSE121690_CpG_genebody_UNmeth_data,
-    GSE121690_CpGgroup,
-    "Developmental_stage","meth","UNmeth",
-    group_suffix)
-  genebody_DEG_data <- merge(genebody_DEG_result, genebody_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
-
-  genebody_file_suffix <- "data/meth_DEG/genebody/"
-  genebody_file_name <- paste0(group_suffix,"_CpG.csv")
-  genebody_file_suffix_name <- paste0(genebody_file_suffix,genebody_file_name)
-
-  # write.csv(genebody_DEG_data,genebody_file_suffix_name,row.names = F)
-
-
   cat("enhancer  ")
   encode_DEG_result <- Meth_group_variance_analysis(
     GSE121690_CpG_encode_meth_data,
     GSE121690_CpG_encode_UNmeth_data,
     GSE121690_CpGgroup,
-    "Developmental_stage","meth","UNmeth",
+    "Developmental_stage","site","UNsite",
     group_suffix)
   encode_DEG_data <- merge(encode_DEG_result, encode_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
@@ -103,7 +87,7 @@ for (i in 1:length(group_levels)) {
     GSE121690_GpC_genetss2k_meth_data,
     GSE121690_GpC_genetss2k_UNmeth_data,
     GSE121690_GpCgroup,
-    "Developmental_stage","meth","UNmeth",
+    "Developmental_stage","site","UNsite",
     group_suffix)
   genetss2k_DEG_data <- merge(genetss2k_DEG_result, genetss2k_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
@@ -111,23 +95,7 @@ for (i in 1:length(group_levels)) {
   genetss2k_file_name <- paste0(group_suffix,"_GpC.csv")
   genetss2k_file_suffix_name <- paste0(genetss2k_file_suffix,genetss2k_file_name)
 
-  write.csv(genetss2k_DEG_data,genetss2k_file_suffix_name,row.names = F)
-
-
-  cat("genebody ")
-  genebody_DEG_result <- Meth_group_variance_analysis(
-    GSE121690_GpC_genebody_meth_data,
-    GSE121690_GpC_genebody_UNmeth_data,
-    GSE121690_GpCgroup,
-    "Developmental_stage","meth","UNmeth",
-    group_suffix)
-  genebody_DEG_data <- merge(genebody_DEG_result, genebody_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
-
-  genebody_file_suffix <- "data/meth_DEG/genebody/"
-  genebody_file_name <- paste0(group_suffix,"_GpC.csv")
-  genebody_file_suffix_name <- paste0(genebody_file_suffix,genebody_file_name)
-
-  write.csv(genebody_DEG_data,genebody_file_suffix_name,row.names = F)
+  # write.csv(genetss2k_DEG_data,genetss2k_file_suffix_name,row.names = F)
 
 
   cat("enhancer  ")
@@ -135,7 +103,7 @@ for (i in 1:length(group_levels)) {
     GSE121690_GpC_encode_meth_data,
     GSE121690_GpC_encode_UNmeth_data,
     GSE121690_GpCgroup,
-    "Developmental_stage","meth","UNmeth",
+    "Developmental_stage","site","UNsite",
     group_suffix)
   encode_DEG_data <- merge(encode_DEG_result, encode_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
@@ -143,7 +111,7 @@ for (i in 1:length(group_levels)) {
   encode_file_name <- paste0(group_suffix,"_GpC.csv")
   encode_file_suffix_name <- paste0(encode_file_suffix,encode_file_name)
 
-  write.csv(encode_DEG_data,encode_file_suffix_name,row.names = F)
+  # write.csv(encode_DEG_data,encode_file_suffix_name,row.names = F)
   cat("The ",group_suffix," ", as.numeric((Sys.time() - start_time),units = "mins"), "mins\n")
 }
 
@@ -152,9 +120,9 @@ for (i in 1:length(group_levels)) {
 
 #methlevel----
 #CpG----
-GSE121690_CpG_genetss2k_methlevel_data <- Read_file_meth_colname(GSE121690_CpG_genetss2k_methlevel,"methlevel")
-GSE121690_CpG_genebody_methlevel_data <- Read_file_meth_colname(GSE121690_CpG_genebody_methlevel,"methlevel")
-GSE121690_CpG_encode_methlevel_data <- Read_file_meth_colname(GSE121690_CpG_encode_methlevel,"methlevel")
+GSE121690_CpG_genetss2k_methlevel_data <- Read_file_meth_colname(GSE121690_CpG_genetss2k_methlevel,"level")
+GSE121690_CpG_genebody_methlevel_data <- Read_file_meth_colname(GSE121690_CpG_genebody_methlevel,"level")
+GSE121690_CpG_encode_methlevel_data <- Read_file_meth_colname(GSE121690_CpG_encode_methlevel,"level")
 
 GSE121690_CpG_genetss2k_methlevel_data <- GSE121690_CpG_genetss2k_methlevel_data/100
 GSE121690_CpG_genebody_methlevel_data <- GSE121690_CpG_genebody_methlevel_data/100
@@ -162,9 +130,9 @@ GSE121690_CpG_encode_methlevel_data <- GSE121690_CpG_encode_methlevel_data/100
 
 
 #GpC----
-GSE121690_GpC_genetss2k_methlevel_data <- Read_file_meth_colname(GSE121690_GpC_genetss2k_methlevel,"methlevel")
-GSE121690_GpC_genebody_methlevel_data <- Read_file_meth_colname(GSE121690_GpC_genebody_methlevel,"methlevel")
-GSE121690_GpC_encode_methlevel_data <- Read_file_meth_colname(GSE121690_GpC_encode_methlevel,"methlevel")
+GSE121690_GpC_genetss2k_methlevel_data <- Read_file_meth_colname(GSE121690_GpC_genetss2k_methlevel,"level")
+GSE121690_GpC_genebody_methlevel_data <- Read_file_meth_colname(GSE121690_GpC_genebody_methlevel,"level")
+GSE121690_GpC_encode_methlevel_data <- Read_file_meth_colname(GSE121690_GpC_encode_methlevel,"level")
 
 GSE121690_GpC_genetss2k_methlevel_data <- GSE121690_GpC_genetss2k_methlevel_data/100
 GSE121690_GpC_genebody_methlevel_data <- GSE121690_GpC_genebody_methlevel_data/100
@@ -181,38 +149,26 @@ for (i in 1:length(group_levels)) {
   cat(group_suffix,"\n")
   cat("genetss2k ")
   genetss2k_DEG_result <- Methlevel_group_variance_analysis(GSE121690_CpG_genetss2k_methlevel_data,GSE121690_CpGgroup,
-                                                            "Developmental_stage","methlevel",group_suffix)
+                                                            "Developmental_stage","level",group_suffix)
   genetss2k_DEG_data <- merge(genetss2k_DEG_result, genetss2k_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
   genetss2k_file_suffix <- "data/methlevel_DEG/genetss2k/"
   genetss2k_file_name <- paste0(group_suffix,"_CpG.csv")
   genetss2k_file_suffix_name <- paste0(genetss2k_file_suffix,genetss2k_file_name)
 
-  write.csv(genetss2k_DEG_data,genetss2k_file_suffix_name,row.names = F)
-
-
-  cat("genebody ")
-  genebody_DEG_result <- Methlevel_group_variance_analysis(GSE121690_CpG_genebody_methlevel_data,GSE121690_CpGgroup,
-                                                           "Developmental_stage","methlevel",group_suffix)
-  genebody_DEG_data <- merge(genebody_DEG_result, genebody_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
-
-  genebody_file_suffix <- "data/methlevel_DEG/genebody/"
-  genebody_file_name <- paste0(group_suffix,"_CpG.csv")
-  genebody_file_suffix_name <- paste0(genebody_file_suffix,genebody_file_name)
-
-  write.csv(genebody_DEG_data,genebody_file_suffix_name,row.names = F)
+  # write.csv(genetss2k_DEG_data,genetss2k_file_suffix_name,row.names = F)
 
 
   cat("enhancer  ")
   encode_DEG_result <- Methlevel_group_variance_analysis(GSE121690_CpG_encode_methlevel_data,GSE121690_CpGgroup,
-                                                         "Developmental_stage","methlevel",group_suffix)
+                                                         "Developmental_stage","level",group_suffix)
   encode_DEG_data <- merge(encode_DEG_result, encode_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
   encode_file_suffix <- "data/methlevel_DEG/enhancer/"
   encode_file_name <- paste0(group_suffix,"_CpG.csv")
   encode_file_suffix_name <- paste0(encode_file_suffix,encode_file_name)
 
-  write.csv(encode_DEG_data,encode_file_suffix_name,row.names = F)
+  # write.csv(encode_DEG_data,encode_file_suffix_name,row.names = F)
   cat("The ",group_suffix," ", as.numeric((Sys.time() - start_time),units = "mins"), "mins\n")
 }
 
@@ -225,37 +181,25 @@ for (i in 1:length(group_levels)) {
   cat(group_suffix,"\n")
   cat("genetss2k ")
   genetss2k_DEG_result <- Methlevel_group_variance_analysis(GSE121690_GpC_genetss2k_methlevel_data,GSE121690_GpCgroup,
-                                                            "Developmental_stage","methlevel",group_suffix)
+                                                            "Developmental_stage","level",group_suffix)
   genetss2k_DEG_data <- merge(genetss2k_DEG_result, genetss2k_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
   genetss2k_file_suffix <- "data/methlevel_DEG/genetss2k/"
   genetss2k_file_name <- paste0(group_suffix,"_GpC.csv")
   genetss2k_file_suffix_name <- paste0(genetss2k_file_suffix,genetss2k_file_name)
 
-  write.csv(genetss2k_DEG_data,genetss2k_file_suffix_name,row.names = F)
-
-
-  cat("genebody ")
-  genebody_DEG_result <- Methlevel_group_variance_analysis(GSE121690_GpC_genebody_methlevel_data,GSE121690_GpCgroup,
-                                                           "Developmental_stage","methlevel",group_suffix)
-  genebody_DEG_data <- merge(genebody_DEG_result, genebody_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
-
-  genebody_file_suffix <- "data/methlevel_DEG/genebody/"
-  genebody_file_name <- paste0(group_suffix,"_GpC.csv")
-  genebody_file_suffix_name <- paste0(genebody_file_suffix,genebody_file_name)
-
-  write.csv(genebody_DEG_data,genebody_file_suffix_name,row.names = F)
+  # write.csv(genetss2k_DEG_data,genetss2k_file_suffix_name,row.names = F)
 
 
   cat("enhancer  ")
   encode_DEG_result <- Methlevel_group_variance_analysis(GSE121690_GpC_encode_methlevel_data,GSE121690_GpCgroup,
-                                                         "Developmental_stage","methlevel",group_suffix)
+                                                         "Developmental_stage","level",group_suffix)
   encode_DEG_data <- merge(encode_DEG_result, encode_data, by.x = "chrdata", by.y = "chrdata", all.x = TRUE)
 
   encode_file_suffix <- "data/methlevel_DEG/enhancer/"
   encode_file_name <- paste0(group_suffix,"_GpC.csv")
   encode_file_suffix_name <- paste0(encode_file_suffix,encode_file_name)
 
-  write.csv(encode_DEG_data,encode_file_suffix_name,row.names = F)
+  # write.csv(encode_DEG_data,encode_file_suffix_name,row.names = F)
   cat("The ",group_suffix," ", as.numeric((Sys.time() - start_time),units = "mins"), "mins\n")
 }
